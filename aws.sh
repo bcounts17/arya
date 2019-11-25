9865a5812cff62f68f03f3cc69727de2bcbeb4e5
bcounts17 <58153455+bcounts17@users.noreply.github.com>
2019-11-25 08:39:05 +0800

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #login to aws using putty without private key..
+
 #sudo bash
 sed -i 's@#PermitRootLogin[[:space:]]prohibit-password@PermitRootLogin yes@g' /etc/ssh/sshd_config
+
 sed -i 's@#PubkeyAuthentication[[:space:]]yes@PubkeyAuthentication no@g' /etc/ssh/sshd_config
+
 sed -i 's@PasswordAuthentication[[:space:]]no@PasswordAuthentication yes@g' /etc/ssh/sshd_config
-service ssh restart
+
+service sshd restart
 passwd root
